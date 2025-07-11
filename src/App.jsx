9f57f898c90b579da91bbfbc3f7e4b1a10b5c26f
Vipul Jhajharia <jhajharia2610@gmail.com>
2025-07-11 13:55:31 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import NavbarMain from './components/navbar/NavbarMain'
+
+function App() {
+  
+
+  return (
+   <main className="font-body">
+    <NavbarMain/>
+   </main>
+  )
+}
+
+export default App
---
+++ src/components/navbar/NavbarLogo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavbarLogo = () => {
+  return (
+    <div>
+       <h1 className="text-white text-2xl">Vipul Jhajharia</h1>
+        </div>
+  )
+}
+
+export default NavbarLogo
